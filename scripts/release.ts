0e42523ca4ba3f7e0b3b949ad4948aee943c0a23
CyFang <chenyuanchn.fang@gmail.com>
2024-09-29 18:06:53 +0800

    fix: test

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,6 +1,54 @@
+import semver from 'semver'
 import { findWorkspacePackages } from '@pnpm/find-workspace-packages'
 
+type releaseType =
+  | 'major'
+  | 'premajor'
+  | 'minor'
+  | 'preminor'
+  | 'patch'
+  | 'prepatch'
+  | 'prerelease'
+
+type SemverRow = {
+  release: releaseType
+  optionsOrLoose?: boolean | string
+  identifier?: string
+}
+
 const cwd = process.cwd()
+
+const versionType: SemverRow = [
+  {
+    release: 'major',
+  },
+  {
+    release: 'premajor',
+  },
+  {
+    release: 'minor',
+  },
+  {
+    release: 'preminor',
+    optionsOrLoose: 'rc',
+    identifier: '1',
+  },
+  {
+    release: 'patch',
+    optionsOrLoose: 'rc',
+    identifier: '1',
+  },
+  {
+    release: 'prepatch',
+    optionsOrLoose: 'rc',
+    identifier: '1',
+  },
+  {
+    release: 'prerelease',
+    optionsOrLoose: 'rc',
+    identifier: '1',
+  },
+]
 
 async function init() {
   const res = await findWorkspacePackages(cwd)
@@ -11,4 +59,6 @@ async function init() {
   console.log(list, '--')
 }
 
+const version = semver.inc('1.2.3', 'prerelease', 'beta')
+console.log(version)
 init()
